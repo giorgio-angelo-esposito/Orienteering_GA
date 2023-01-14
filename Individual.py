809ebc4e3777a7ee44383ac7e067b67a35f21f5c
giorgio-angelo-esposito <92090977+giorgio-angelo-esposito@users.noreply.github.com>
2023-01-14 21:57:15 +0100

    Update Individual.py
    Added the compass function and  the number of control point reached as a variable

--- Individual.py
+++ Individual.py
@@ -15,6 +15,7 @@ class Individual:
     '''
     self.path = []
     self.fitness = 0
+    self.number_of_cp_reached = 0
 
   def getPath(self):
     # this method is used to retrieve the path of the individual 
@@ -23,3 +24,24 @@ class Individual:
   def getFitness(self):
     # this method is used to retrieve the fitness of the individual
     return self.fitness
+
+  def compass(self,current_point,control_point):
+    
+    if current_point[0] >= control_point[0] and current_point[1] >= control_point[1]:
+      direction1 = 0
+      direction2 = 1
+    elif current_point[0] >= control_point[0] and current_point[1] <= control_point[1]:
+      direction1 = 1
+      direction2 = 2
+    elif current_point[0] <= control_point[0] and current_point[1] <= control_point[1]:
+      direction1 = 2
+      direction2 = 3
+    elif current_point[0] <= control_point[0] and current_point[1] >= control_point[1]:
+      direction1 = 0
+      direction2 = 3
+
+
+    return direction1, direction2
+
+  def __repr__(self):
+        return "(Path: " + str(self.path) + ", fitness: " + str(self.fitness) + ", number_of_cp_reached: "+ str(self.number_of_cp_reached) + ")" self.fitness
